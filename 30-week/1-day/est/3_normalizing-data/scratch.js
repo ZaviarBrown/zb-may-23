e2071b9ddb1978caf30d6e34722af4da4b523936
Zaviar Brown <brown.zaviar@gmail.com>
2023-12-04 19:00:39 -0700

    Third practice walkthrough complete

---
+++ 30-week/1-day/est/3_normalizing-data/scratch.js
@@ -0,0 +1,26 @@
+//! --------------------------------------------------------------------
+//*                            Different Arrays
+//! --------------------------------------------------------------------
+
+const giveMeArray = () => ['hey'];
+const myArr1 = giveMeArray();
+const myArr2 = giveMeArray();
+
+console.log(myArr1, myArr2);
+
+myArr1.push('bye');
+
+console.log(myArr1, myArr2);
+
+//! --------------------------------------------------------------------
+//*                          Same array
+//! --------------------------------------------------------------------
+
+// const myArr1 = ['hey'];
+// const myArr2 = myArr1;
+
+// console.log(myArr1, myArr2);
+
+// myArr1.push('bye');
+
+// console.log(myArr1, myArr2);
